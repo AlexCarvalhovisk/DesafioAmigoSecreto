e68d57bd27a29351b40fab0fddedea352b237de1
Alex <adscarvalho@restinga.ifrs.edu.br>
2025-08-08 00:20:36 -0300

    Criação de variáveis, funções para add e exibir texto.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+const input = document.getElementById('amigo');
+const mensagem = document.getElementById('mensagem');
+
+function adicionarAmigo() {
+    const nome = input.value.trim();
+
+    if (nome === '') {
+        exibirMensagem('Por favor, digite o nome do amigo.', 'erro');
+        return;
+    }
+
+    if (listaDeAmigos.includes(nome)) {
+        exibirMensagem('Este nome já foi adicionado!', 'erro');
+        input.value = '';
+        return;
+    }
+
+    //Aqui adiciono o amigo na lista.
+    listaDeAmigos.push(nome);
+    input.value = '';
+    input.focus();
+    //console.log(listaDeAmigos);
+    //mostrarListaTela();
+    //exibirMensagem(`Amigo ${nome} foi adicionado com sucesso!`, 'sucesso');
+}
+
+//Aqui manipulei a mensagem pra aparecer erro ou sucesso na cor que eu quero na tela.
+function exibirMensagem(texto, tipo) {
+    mensagem.textContent = texto;
+    mensagem.className = `mensagem ${tipo}`;
+}
